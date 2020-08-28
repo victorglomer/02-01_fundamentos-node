e48f6c23c9c797e61bf61f9baaa1f6feb428ca26
Victor Hugo Sanches Rodrigues <victor.rodrigues@etapa.com.br>
2020-08-28 15:47:03 -0300

    Finalizado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,10 @@ import transactionRouter from './transaction.routes';
 
 const routes = Router();
 
+routes.get('/teste', (request, response) => {
+  return response.send('jjj');
+});
+
 routes.use('/transactions', transactionRouter);
 
 export default routes;
